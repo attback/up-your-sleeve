802d146f3c92121faf928492acb8e0b335c2b265
Josefin Attback <josefin.attback@xdin.com>
2013-12-18 10:23:19 +0100

    Generate pages_controller (which also creates routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 UpYourSleeve::Application.routes.draw do
+  get "pages/home"
+  get "pages/about"
+  get "pages/wewiki"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
