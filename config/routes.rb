91e3a03ac57ec97f1a8d2b29fde39211b9929168
Josefin Attback <josefin.attback@xdin.com>
2013-12-18 14:01:50 +0100

    Added slightly dynamic titles

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 UpYourSleeve::Application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/wewiki"
+
+get "pages/home"
+get "pages/about"
+get "pages/wewiki"
+
+
+  # # You can have the root of your site routed with "root"
+  # root 'pages#home'
+  
+  # match '/about',   to: 'pages#about',   via: 'get'
+  # match '/wewiki', to: 'pages#wewiki', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
