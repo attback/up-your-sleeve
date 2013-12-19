25b0a42aad3ce18a423cfa9f5f884c83960d2ac1
Josefin Attback <josefin.attback@xdin.com>
2013-12-19 11:05:18 +0100

    Changed link to background image to get it to work after deploy to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ UpYourSleeve::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true 
+  config.serve_static_assets = true
   # Josefin
 
   # Compress JavaScripts and CSS.
